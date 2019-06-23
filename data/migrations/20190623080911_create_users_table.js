d414ca0a3e8c590d486d4cbc29bce98983ea5e30
Ming Liu <mliu@macpro.home>
2019-06-23 13:13:56 -0400

    migrate

--- data/migrations/20190623080911_create_users_table.js
+++ data/migrations/20190623080911_create_users_table.js
@@ -5,6 +5,11 @@ exports.up = function(knex, Promise) {
       table.increments();
       table.string("username", 256).notNullable().unique();
       table.string("password", 256).notNullable()
+      table.string("name", 256)
+      table.integer("age", 2).unsigned()
+      table.string("title", 256)
+      table.string("tagline", 512)
+      table.integer("yearsAsGuide", 2) //experience as a guide in years
   })
 };
 
--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -4,9 +4,28 @@ exports.seed = function(knex, Promise) {
   return knex('users').truncate()
     .then(function () {
       return knex('users').insert([
-        {username: 'backpackHero', password: bcrypt.hashSync("1234", 12)},
-        {username: 'scubascuba', password: bcrypt.hashSync("1234", 12)},
-        {username: 'gr8Guide', password: bcrypt.hashSync("1234", 12)}
+        {
+          username: 'backpackHero', 
+          password: bcrypt.hashSync("1234", 12), 
+          name: 'Janie Pro',
+          age: 28,
+          title: 'Professional Backpack Guide',
+          tagline: 'I\'m the all around girl who loves backpacking',
+          yearsAsGuide: 6
+        },
+        {
+          username: 'scubascuba', 
+          password: bcrypt.hashSync("1234", 12),
+          name: 'Bill Water',
+          age: 32,
+          title: 'Professional Scuba Diver',
+          tagline: 'Absolutely the best scuba diver in the world!',
+          yearsAsGuide: 5
+        },
+        {
+          username: 'gr8Guide', 
+          password: bcrypt.hashSync("1234", 12)
+        }
       ]);
     });
 };
